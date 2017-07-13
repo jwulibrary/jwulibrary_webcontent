0c121433460c14b95676b7877d53b72b16571561
David <dmeincke@gmail.com>
2017-07-13 14:37:53 -0400

    Added SVG Social icons to footer

--- main.js
+++ main.js
@@ -192,58 +192,58 @@ allFaqs = ["How can I get access to EasyBib EDU?", "Do you have any resources to
 
 
 
-
-    definegrid = function() {
-        var browserWidth = $(window).width();
-        if (browserWidth >= 1001)
-        {
-            pageUnits = 'px';
-            colUnits = 'px';
-            pagewidth = 960;
-            columns = 6;
-            columnwidth = 140;
-            gutterwidth = 24;
-            pagetopmargin = 35;
-            rowheight = 20;
-            gridonload = 'off';
-            makehugrid();
-        }
-        if (browserWidth <= 1000)
-        {
-            pageUnits = '%';
-            colUnits = '%';
-            pagewidth = 94;
-            columns = 2;
-            columnwidth = 48;
-            gutterwidth = 4;
-            pagetopmargin = 35;
-            rowheight = 20;
-            gridonload = 'off';
-            makehugrid();
-        }
-        if (browserWidth <= 768)
-        {
-            pageUnits = '%';
-            colUnits = '%';
-            pagewidth = 96;
-            columns = 2;
-            columnwidth = 49;
-            gutterwidth = 2;
-            pagetopmargin = 35;
-            rowheight = 20;
-            gridonload = 'off';
-            makehugrid();
-        }
-    }
-    $(document).ready(function() {
-        definegrid();
-        setgridonload();
-    });
-
-    $(window).resize(function() {
-        definegrid();
-        setgridonresize();
-    });
+    // 
+    // definegrid = function() {
+    //     var browserWidth = $(window).width();
+    //     if (browserWidth >= 1001)
+    //     {
+    //         pageUnits = 'px';
+    //         colUnits = 'px';
+    //         pagewidth = 960;
+    //         columns = 6;
+    //         columnwidth = 140;
+    //         gutterwidth = 24;
+    //         pagetopmargin = 35;
+    //         rowheight = 20;
+    //         gridonload = 'off';
+    //         makehugrid();
+    //     }
+    //     if (browserWidth <= 1000)
+    //     {
+    //         pageUnits = '%';
+    //         colUnits = '%';
+    //         pagewidth = 94;
+    //         columns = 2;
+    //         columnwidth = 48;
+    //         gutterwidth = 4;
+    //         pagetopmargin = 35;
+    //         rowheight = 20;
+    //         gridonload = 'off';
+    //         makehugrid();
+    //     }
+    //     if (browserWidth <= 768)
+    //     {
+    //         pageUnits = '%';
+    //         colUnits = '%';
+    //         pagewidth = 96;
+    //         columns = 2;
+    //         columnwidth = 49;
+    //         gutterwidth = 2;
+    //         pagetopmargin = 35;
+    //         rowheight = 20;
+    //         gridonload = 'off';
+    //         makehugrid();
+    //     }
+    // }
+    // $(document).ready(function() {
+    //     definegrid();
+    //     setgridonload();
+    // });
+    //
+    // $(window).resize(function() {
+    //     definegrid();
+    //     setgridonresize();
+    // });
 
 
 
@@ -251,4 +251,7 @@ allFaqs = ["How can I get access to EasyBib EDU?", "Do you have any resources to
 $("#s-la-public-header").hide();
 $("#s-la-page-title-bar").hide();
 $('#s-la-bc').hide();
+
+
+
 })
